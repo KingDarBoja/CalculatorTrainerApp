af1ecda4fae485c4e2013b614c0587b4128353c8
KingDarBoja <mbojato@uninorte.edu.co>
2017-11-13 18:06:25 -0500

    Agregar archivos de proyecto.

---
+++ CalculatorTrainerApp/CalculatorTrainerApp/MainGame.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorTrainerApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainGame : ContentPage
+    {
+        int counter = 0;
+        int okAnsCount = 0;
+        int finalValue;
+        Random randNumber1 = new Random();
+        Random randNumber2 = new Random();
+        Random randOp = new Random();
+        public MainGame(String value)
+        {
+            Title = "Operando ando";
+            InitializeComponent();
+            NumberTest.Text = "Numero de pruebas: " + value;
+
+            NextBtn.Clicked += delegate {
+
+                if (AnswerEntry.Text == finalValue.ToString())
+                {
+                    okAnsCount = okAnsCount + 1;
+                    DisplayAlert(" Excelente! ", "Respuestas correctas hasta ahora: " + okAnsCount.ToString(), "OK");
+                }
+                else
+                {
+                    DisplayAlert(" Fallaste! ", "Respuesta Incorrectas", "OK");
+                }
+                int x = randOp.Next(0, 2);
+                AnswerEntry.Text = "";
+                counter = counter + 1;
+                CounterTest.Text = "Prueba N° " + counter;
+                FirstNumber.Text = randNumber1.Next(0, 100).ToString();
+                SecondNumber.Text = randNumber2.Next(0, 100).ToString();
+                if (x == 0)
+                {
+                    OperationRnd.Text = "+";
+                    finalValue = AddTwoStrings(FirstNumber.Text, SecondNumber.Text);
+                }
+                else
+                {
+                    OperationRnd.Text = "-";
+                    finalValue = SubsTwoStrings(FirstNumber.Text, SecondNumber.Text);
+                }
+            };
+        }
+
+
+        private static int AddTwoStrings(string one, string two)
+        {
+            Int32.TryParse(one, out int iOne);
+            Int32.TryParse(two, out int iTwo);
+            return iOne + iTwo;
+        }
+
+        private static int SubsTwoStrings(string one, string two)
+        {
+            Int32.TryParse(one, out int iOne);
+            Int32.TryParse(two, out int iTwo);
+            return iOne - iTwo;
+        }
+    }
+}
---
+++ CalculatorTrainerApp/CalculatorTrainerApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculatorTrainerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Title = "Calculator Trainer";
+            InitializeComponent();
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void StartGameBtnOnClick(object sender, EventArgs e)
+        {
+            var numberTestLabel = new Label();
+            numberTestLabel.SetBinding(Label.TextProperty, new Binding("SelectedItem", source: NumberQuestion));
+            Navigation.PushAsync(new MainGame(numberTestLabel.Text.ToString()));
+        }
+    }
+}
